eb90f7f50346bd90419e998310abf9ded5471439
Philippe Vosges <philippe.vosges@gmail.com>
2011-03-04 10:01:55 +0100

    added wrapper to avoid conflicts with non-jquery libs

--- jquery.observe_field.js
+++ jquery.observe_field.js
@@ -1,32 +1,38 @@
 // jquery.observe_field.js
 
-jQuery.fn.observe_field = function(frequency, callback) {
 
-  return this.each(function(){
-    var $this = $(this);
-    var prev = $this.val();
+(function( $ ){
 
-    var check = function() {
-      var val = $this.val();
-      if(prev != val){
-        prev = val;
-        $this.map(callback); // invokes the callback on $this
-      }
-    };
+  jQuery.fn.observe_field = function(frequency, callback) {
 
-    var reset = function() {
-      if(ti){
-        clearInterval(ti);
-        ti = setInterval(check, frequency);
-      }
-    };
+    return this.each(function(){
+      var $this = $(this);
+      var prev = $this.val();
 
-    check();
-    frequency = frequency * 1000; // translate to milliseconds
-    var ti = setInterval(check, frequency); // invoke check periodically
+      var check = function() {
+        var val = $this.val();
+        if(prev != val){
+          prev = val;
+          $this.map(callback); // invokes the callback on $this
+        }
+      };
 
-    // reset counter after user interaction
-    $this.bind('keyup mousemove', reset); //mousemove is for selects
-  });
+      var reset = function() {
+        if(ti){
+          clearInterval(ti);
+          ti = setInterval(check, frequency);
+        }
+      };
 
-};
+      check();
+      frequency = frequency * 1000; // translate to milliseconds
+      var ti = setInterval(check, frequency); // invoke check periodically
+
+      // reset counter after user interaction
+      $this.bind('keyup mousemove', reset); //mousemove is for selects
+    });
+
+  };
+
+})( jQuery );
+
