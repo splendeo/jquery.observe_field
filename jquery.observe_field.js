4b16f102c8ca58ceffb3d2ca18c74c4a7da14856
Enrique García <kikito@gmail.com>
2011-03-04 09:35:59 +0100

    replaced element by $this

--- jquery.observe_field.js
+++ jquery.observe_field.js
@@ -4,21 +4,21 @@
 jQuery.fn.observe_field = function(frequency, callback) {
 
   return this.each(function(){
-    var element = $(this);
-    var prev = element.val();
+    var $this = $(this);
+    var prev = $this.val();
 
     var chk = function() {
-      var val = element.val();
+      var val = $this.val();
       if(prev != val){
         prev = val;
-        element.map(callback); // invokes the callback on the element
+        $this.map(callback); // invokes the callback on $this
       }
     };
     chk();
     frequency = frequency * 1000; // translate to milliseconds
     var ti = setInterval(chk, frequency);
     // reset counter after user interaction
-    element.bind('keyup', function() {
+    $this.bind('keyup', function() {
       ti && clearInterval(ti);
       ti = setInterval(chk, frequency);
     });
